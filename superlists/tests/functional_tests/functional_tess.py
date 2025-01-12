5419d9defd6dc8cbaf4795afefac109e2c6cb17f
Olivier <oliviersamin@gmail.com>
2025-01-12 21:58:02 +0100

    update the FT with unitest class to improve test results readability

--- superlists/tests/functional_tests/functional_tess.py
+++ superlists/tests/functional_tests/functional_tess.py
@@ -1,11 +1,30 @@
+"""
+As a user,
+I want to access a web application hosted on my local server,
+So that I can verify that the application is installed and running successfully.
+"""
+
 from selenium import webdriver
+import unittest
 from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.firefox.options import Options
 
-options = Options()
 
-service = Service("/snap/bin/geckodriver")
-browser = webdriver.Firefox(service=service, options=options)
+class NewVisitorTest(unittest.TestCase):
 
-browser.get("http://localhost:8000")
-assert "The install worked successfully! Congratulations!" in browser.title
+    def setUp(self):
+        self.options = Options()
+        self.service = Service("/snap/bin/geckodriver")
+        self.browser = webdriver.Firefox(service=self.service, options=self.options)
+
+    def tearDown(self) -> None:
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.browser.get("http://localhost:8000")
+        self.assertIn("To-do", self.browser.title)
+        self.fail("Test to be finished!")
+
+
+if __name__ == "__main__":
+    unittest.main(warnings="ignore")
