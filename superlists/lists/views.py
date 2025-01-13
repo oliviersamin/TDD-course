7bfcd2cad45d97b2b1f8e8943414493695ed0465
Olivier <oliviersamin@gmail.com>
2025-01-13 23:23:27 +0100

    update unit test to test homepage template logic

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -3,17 +3,5 @@ from django.http import HttpResponse
 
 
 def home_page(request):
-    template = "superlists/lists/templates/home_page.html"
-
-    body = """<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>To-Do lists</title>
-</head>
-<body>
-
-</body>
-</html>"""
-
-    return HttpResponse(body)
+    template = "home.html"
+    return render(request, template)
